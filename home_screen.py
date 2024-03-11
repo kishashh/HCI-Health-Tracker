22ea9e738f80d6492b174bd45fa937c70a942ea0
Dennis J Case JR <denniscase1999@gmail.com>
2024-03-11 18:40:25 -0500

    Update home_screen.py
    Prepping code for next file

--- home_screen.py
+++ home_screen.py
@@ -27,17 +27,27 @@ class NutritionTracker(tk.Tk):                  # Main class for application
         add_food_button = tk.Button(self, text="Add Food", command=self.to_adding_food)   # Button to add food
         add_food_button.pack(pady=(10,20))      # Add some padding
 
+        add_food_button = tk.Button(self, text="Show Progress", command=self.to_show_progress)   # Button to add food
+        add_food_button.pack(pady=(10,20))      # Add some padding
+
     # Function that clears screen to allow new variables to be used
     #def clear_screen(self):
     #    for widget in self.winfo_children():    # Calls on all window children variables
     #        widget.destroy()                    # Destroys all window children variables
 
-    def to_adding_food(self):
+    def to_adding_food(self):                   # Def to delete screen and start add_food screen
         #self.clear_screen()
         #self = AddingFood.self
-        self.destroy()
-        main_window = add_food.AddingFood()
-        main_window.mainloop()
+        self.destroy()                          # Delete current screen
+        main_window = add_food.AddingFood()     # Starts add_food
+        main_window.mainloop()                  # Starts loop for window
+
+    def to_show_progress(self):                 # Def to delete screen and start  screen
+        #self.clear_screen()
+        #self = AddingFood.self
+        self.destroy()                          # Delete current screen
+        main_window = add_food.AddingFood()     # Starts add_food
+        main_window.mainloop()                  # Starts loop for window
 
 
 
